5f3e98dd78ff5af3734e6c74384b95c82603201c
ilyaistomin789 <ilyaistomin.888@yandex.ru>
2024-03-26 15:03:18 +0300

    Fixed AuthService, UseLogin hook

--- OsuMapFinder/OsuMapFinder.Client/src/app/pages/Auth/Auth.tsx
+++ OsuMapFinder/OsuMapFinder.Client/src/app/pages/Auth/Auth.tsx
@@ -1,35 +1,51 @@
-import UseLogin from '../../shared/components/apiService/AuthService';
-import LoginFormElement from '../../shared/consts/models/formElements/LoginElement';
-import './Auth.scss'
+import UseLogin from "../../shared/components/apiService/AuthService";
+import LoginFormElement from "../../shared/consts/models/formElements/LoginElement";
+import "./Auth.scss";
 
 const Auth = () => {
-    const loginQuery = (event: React.FormEvent<LoginFormElement>) => {
-        event.preventDefault();
-        
-        const loginQuery = UseLogin(event.currentTarget.elements.username.value, event.currentTarget.elements.password.value);
-        console.log(loginQuery.data)
-    };
-    return (
-        <div className="login-form">
-            <form onSubmit={loginQuery}>
-                <h2>Osu Map Finder</h2>
-                <p className='mb-5'>Some desc</p>
-                <div className="mb-3">
-                    <input className='input-form' type='text' name='username' placeholder="Username" />
-                </div>
-                <div className="mb-3">
-                    <input className='input-form' type="password" name='password' placeholder="Password" />
-                </div>
-                <div className="check-form mb-4">
-                    <input type='checkbox' className='form-check-input mt-0 checkbox' value='' id='checkbox-input'/>
-                    <label htmlFor="checkbox-input" className='ms-2'>Check me out</label>
-                </div>
-                <button className='button' type="submit">
-                    Submit
-                </button>
-            </form>
+  const { mutateAsync } = UseLogin();
+  const handleSubmit = (event: React.FormEvent<LoginFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="login-form">
+      <form onSubmit={handleSubmit}>
+        <h2>Osu Map Finder</h2>
+        <p className="mb-5">Some desc</p>
+        <div className="mb-3">
+          <input
+            className="input-form"
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
         </div>
-    );
-}
+        <div className="mb-3">
+          <input
+            className="input-form"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="check-form mb-4">
+          <input
+            type="checkbox"
+            className="form-check-input mt-0 checkbox"
+            value=""
+            id="checkbox-input"
+          />
+          <label htmlFor="checkbox-input" className="ms-2">
+            Check me out
+          </label>
+        </div>
+        <button className="button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
--- OsuMapFinder/OsuMapFinder.Client/src/app/shared/components/apiService/AuthService.tsx
+++ OsuMapFinder/OsuMapFinder.Client/src/app/shared/components/apiService/AuthService.tsx
@@ -1,17 +1,36 @@
-import { UseMutationResult, useMutation } from "@tanstack/react-query";
+import {
+  MutationFunction,
+  UseMutationResult,
+  useMutation,
+} from "@tanstack/react-query";
 import FetchService from "../FetchService";
 import RequestMethod from "../../consts/enums/RequestMethod";
 import RequestData from "../../consts/models/request/RequestData";
 
-export const LOGIN_KEY = 'login' 
-export const LoginUrl = 'https://localhost:7107/api/Login'
+export const LOGIN_KEY = "login";
+export const LoginUrl = "https://localhost:7107/api/Login";
 
-const UseLogin = (): UseMutationResult<string | null, unknown, string> => {
-    const loginModel = ;
-    const requestModel = ;
-    return useMutation<string | null, unknown, string>({
-        mutationFn: async (username: string | null, password: string | null) => await FetchService(LoginUrl, new RequestData(RequestMethod.POST, JSON.stringify({username: username, password: password})))
-    });
+interface UseLoginBody {
+  username: string | null;
+  password: string | null;
+}
+
+const UseLogin = (): UseMutationResult<
+  string | null,
+  unknown,
+  UseLoginBody
+> => {
+  return useMutation<string | null, unknown, UseLoginBody>({
+    mutationFn: async ({ username, password }: UseLoginBody) =>
+      (await FetchService(
+        LoginUrl,
+        RequestMethod.POST,
+        new RequestData(
+          RequestMethod.POST,
+          JSON.stringify({ username: username, password: password })
+        )
+      )) as Promise<string | null>,
+  });
 };
 
 export default UseLogin;
